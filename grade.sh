a2a72f81c9c88dda04e80820c4216c8bfd37c379
jil258 <102570197+jil258@users.noreply.github.com>
2023-11-29 16:48:35 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,16 +1,47 @@
+
+
+# Define the classpath
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+# Clean up previous grading data
 rm -rf student-submission
 rm -rf grading-area
 
+# Create a fresh grading area
 mkdir grading-area
 
+# Clone the student's submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ ! -f "student-submission/ListExamples.java" ]; then 
+    echo "file not found."
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+# Move the student's code to the grading area
+cp student-submission/* grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area/
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+
+# Navigate to the grading area
+cd grading-area || { echo "Failed to navigate to grading area."; exit 1; }
+
+# Compile the student's code and the test file
+javac -cp "$CPATH" *.java || { echo "Compilation failed"; exit 1; }
+java -cp "$CPATH" org.junit.runner.JUnitCore TestListExamples > test_results.txt
+
+
+# Analyze test results and provide feedback
+if grep -q -i "OK" test_results.txt; then
+    echo "All passed"
+elif grep -q -i "Failures:" test_results.txt; then
+    echo "Some tests failed"
+elif grep -q -i "Errors:" test_results.txt; then
+    echo "There were errors when running the tests."
+else
+    echo "There was a problem running the tests. Check the test file and your code for issues."
+fi
+
+exit 0
